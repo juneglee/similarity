56180f581e7813611711d473327ea8ef8b16d39b
juneglee <klcpop1@gmail.com>
2022-07-14 14:46:08 +0900

    [add] hamming distance

--- feature_extracton.py
+++ feature_extracton.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import torch
 from torchvision import transforms
-from similarity import cosine_similarity, euclidean
+from similarity import cosine_similarity, euclidean, read_img, img2hash, hamming_distance
 
 # Model loading.
 model = torch.jit.load('weight/inception_v3.pt')
@@ -23,15 +23,23 @@ def embedding(image_path):
 
     return embedding
 
+def hammingDistance(target, in_img):
+    targetImage = img2hash(read_img(target))
+    inputImage = img2hash(read_img(in_img))
+    dst = hamming_distance(targetImage, inputImage)
+    if dst/256 < 0.25:
+        return dst/256
+
 if __name__ == '__main__':
     target_path = 'images/cat.jpeg'
-    input_path = 'images/dog.jpeg'
+    input_path = 'images/cat.jpeg' # single or multi
     target_feature = embedding(target_path)
     input_feature = embedding(input_path)
     cos_sim = cosine_similarity(target_feature, input_feature)
     euc_di = euclidean(target_feature, input_feature)
+    ham_di = hammingDistance(target_path, input_path)
 
     print(f'target : {target_path.split("/")[-1]}, input : {input_path.split("/")[-1]}, Cosine similarity : {cos_sim}')
     print(f'target : {target_path.split("/")[-1]}, input : {input_path.split("/")[-1]}, euclidean distance : {euc_di}')
-
+    print(f'target : {target_path.split("/")[-1]}, input : {input_path.split("/")[-1]}, hamming distance : {ham_di}')
 
--- similarity.py
+++ similarity.py
@@ -1,8 +1,38 @@
+import os
+import cv2
 import numpy as np
 from scipy.spatial import distance
 
+# cosine_similarity
 def cosine_similarity(x, y):
     return np.dot(x, y) / (np.sqrt(np.dot(x, x)) * np.sqrt(np.dot(y, y)))
 
+# euclidean
 def euclidean(x, y):
     return distance.euclidean(x, y)
+
+# hamming distance
+def read_img(path):
+    if os.path.isfile(path):
+        return cv2.imread(path, 0)
+    else:
+        raise ValueError('Path provide is not a valid file : {}'.format(path))
+
+def img2hash(img):
+    '''
+    :param img: for binary
+    :return: for binary
+    '''
+    gray = cv2.resize(img, (224, 224))
+    avg = gray.mean()
+    binary = 1 * (gray > avg)
+    return binary
+
+def hamming_distance(a, b):
+    a = a.reshape(1, -1)
+    b = b.reshape(1, -1)
+    # 같은 공간에서의 (0, 1)로 서로 다른 것들을 비교 후 합산한 값
+    distance = (a !=b).sum()
+    return distance
+
+
